7e033aa25c4c08df299cc7caf99417a33d181b22
Gaurav <gaurav.tiwari3001@gmail.com>
2022-08-04 16:27:33 +0530

    project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Footer(){
+
+const year = new Date().getFullYear();
+
+    return(
+        <footer>
+            <p>Made with ❤ by Gaurav</p>
+        </footer>
+    )
+}
+
+export default Footer;
